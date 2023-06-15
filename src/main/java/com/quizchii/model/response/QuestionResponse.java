9c17572d1e82e6855a5744900555793deae174c3
Lam <thaohsk@gmail.com>
2023-06-15 22:16:51 +0700

    Fix get list question

--- src/main/java/com/quizchii/model/response/QuestionResponse.java
+++ src/main/java/com/quizchii/model/response/QuestionResponse.java
@@ -20,6 +20,6 @@ public class QuestionResponse {
     private String answer2;
     private String answer3;
     private String answer4;
-    private String correctAnswer;
+    private Integer correctAnswer;
     private List<TagEntity> tagList;
 }
