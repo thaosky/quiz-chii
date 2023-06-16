b4fce8847af69ca135dd99433c51bdc50b27c19a
Lam <thaohsk@gmail.com>
2023-06-16 22:08:00 +0700

    Edit type question

--- src/main/java/com/quizchii/model/request/QuestionRequest.java
+++ src/main/java/com/quizchii/model/request/QuestionRequest.java
@@ -20,6 +20,6 @@ public class QuestionRequest {
     private String answer2;
     private String answer3;
     private String answer4;
-    private String correctAnswer;
+    private Integer correctAnswer;
     private List<TagEntity> tagList;
 }
