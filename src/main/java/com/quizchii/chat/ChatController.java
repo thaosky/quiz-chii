07e756dadc42396bd209aa85536f380d62803c0e
Lam <thaohsk@gmail.com>
2023-06-24 23:20:10 +0700

    Get name login

--- src/main/java/com/quizchii/chat/ChatController.java
+++ src/main/java/com/quizchii/chat/ChatController.java
@@ -5,6 +5,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,15 +16,16 @@ public class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        log.info("test {}", chatMessage);
+        log.info("{}", chatMessage.getSender());
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
-        log.info("test2 {}", chatMessage);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        chatMessage.setSender(authentication.getName());
+        log.info("============= SENDER {}", chatMessage);
 
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
