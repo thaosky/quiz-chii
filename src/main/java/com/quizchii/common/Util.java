7a057bde5f371b826039309a98b8d20a79065436
Lam <thaohsk@gmail.com>
2024-01-14 23:35:43 +0700

    Calculate streak days

--- src/main/java/com/quizchii/common/Util.java
+++ src/main/java/com/quizchii/common/Util.java
@@ -5,6 +5,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Util {
     public static Timestamp convertTimestampToString(String strDate) {
@@ -18,5 +19,14 @@ public class Util {
             System.out.println("Exception :" + e);
             return null;
         }
+    }
+
+    static long MILLIS_PER_DAY = 1000 * 60 * 60 * 24;
+    public static int daysBetween(long time1, long time2) {
+        // Set both times to 0:00:00
+        time1 -= time1 % MILLIS_PER_DAY;
+        time2 -= time2 % MILLIS_PER_DAY;
+        int res = (int) TimeUnit.DAYS.convert(time1 - time2, TimeUnit.MILLISECONDS);
+        return Math.abs(res);
     }
 }
--- src/main/java/com/quizchii/service/AchievementService.java
+++ src/main/java/com/quizchii/service/AchievementService.java
@@ -1,6 +1,10 @@
 package com.quizchii.service;
 
 import com.quizchii.Enum.SortDir;
+import com.quizchii.common.BusinessException;
+import com.quizchii.common.MessageCode;
+import com.quizchii.common.Util;
+import com.quizchii.entity.UserEntity;
 import com.quizchii.model.ListResponse;
 import com.quizchii.model.response.UserAchievementResponse;
 import com.quizchii.model.view.UserAchievementView;
@@ -13,10 +17,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @AllArgsConstructor
@@ -28,6 +38,8 @@ public class AchievementService {
 
     public ListResponse<UserAchievementResponse> getAchievementByUserId(Long userId, Integer pageSize, Integer pageNo, String sortName, String sortDir) {
         ListResponse<UserAchievementResponse> response = new ListResponse();
+
+        create(userId);
 
         // Paging
         Sort sortable = Sort.by("id").descending();
@@ -44,7 +56,7 @@ public class AchievementService {
         List<UserAchievementResponse> questionResponseList = new ArrayList<>();
 
         // Mapping
-        for (UserAchievementView view: list) {
+        for (UserAchievementView view : list) {
             UserAchievementResponse userAchievementResponse = new UserAchievementResponse();
             BeanUtils.copyProperties(view, userAchievementResponse);
             questionResponseList.add(userAchievementResponse);
@@ -58,12 +70,21 @@ public class AchievementService {
         return response;
     }
 
-//    // Xem có nhận đc phần thưởng hay không
-//    public boolean create(Long userId) {
-//        Optional<UserEntity> optional = userRepository.findById(userId);
-//        UserEntity userEntity = optional.orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, MessageCode.USER_NOT_EXIST));
-//        Timestamp lastActive = userEntity.getLastActive();
-//        Timestamp
-//
-//    }
+    // Xem da hoc bao nhieu ngay lien tiep
+    public int create(Long userId) {
+        Optional<UserEntity> optional = userRepository.findById(userId);
+        UserEntity userEntity = optional.orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, MessageCode.USER_NOT_EXIST));
+
+        // Neu user chua hoc ngay nao => Set streak = 1
+        if (userEntity.getLastActive() == null) {
+            return 1;
+        }
+        long time1 = userEntity.getLastActive().getTime(); // (Mon 31 December 2018 13:19:25)
+        long time2 = new Timestamp(System.currentTimeMillis()).getTime();
+
+        int res = Util.daysBetween(time1, time2);
+        return res + 1;
+    }
+
+
 }
